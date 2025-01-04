f62b9391004581a99650a294baa5d9903a6f66be
yuta asakura <yuta.asakura.se@gmail.com>
2025-01-04 23:13:26 +0900

    initial commit

---
+++ src/main/java/com/concordia/concordia_ems/ConcordiaEmsApplication.java
@@ -0,0 +1,13 @@
+package com.concordia.concordia_ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcordiaEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcordiaEmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concordia/concordia_ems/ConcordiaEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concordia.concordia_ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordiaEmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
